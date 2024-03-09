62a5cc761ea8acc482fb280defdd00ef91b204ac
hasanruhan1998 <158472782+hasanruhan1998@users.noreply.github.com>
2024-03-09 14:55:55 +0600

    lo

---
+++ regester form.html/submit form/form.js
@@ -0,0 +1,47 @@
+function submitForm(event) {
+    event.preventDefault();
+
+
+    let fName = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    // let   Hobby =document.querySelectorAll('input[name"Hobby"]checked');
+
+
+// logic start
+if ( fName==''){
+    alert('j  jg fdkfdkl.');
+    return;
+
+}
+else if (fName.length<=3){
+    alert ('fjdskdndvblk');
+    return;
+}
+
+const regEx= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(regEx.){
+
+}
+   
+   
+   
+   
+   
+    let outPut = 'Name:' + fName + " \n";
+    outPut += 'email:' + email + " \n";
+    outPut += 'password:' + password + " \n";
+
+
+
+
+
+
+
+
+    let newWindow = window.open('', '_blank');
+    newWindow.document.write('<pre>' + outPut + '</pre>');
+}
+
+let myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', submitForm);
